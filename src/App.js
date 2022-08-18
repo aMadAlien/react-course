da4d892d723c9d25b0e9e824f7bb6b74b6f55e61
iren <irasibil2@gmail.com>
2022-08-18 15:02:14 +0300

    search optimizations

--- src/App.js
+++ src/App.js
@@ -22,10 +22,23 @@ class App extends Component {
       ))
   }
 
+  // changes search state
+  onSearchChange = (e) => {
+    const searchField = e.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  }
+
+  
   render() {
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    // array of filtered (searching) monsters
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -34,12 +47,7 @@ class App extends Component {
           className='search-box' 
           type="search" 
           placeholder='search monsters' 
-          onChange={(e) => {
-            const searchField = e.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
