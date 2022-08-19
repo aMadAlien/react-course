f9b2a2e4b59313fa8f5d3ae8176e026f44a3fed1
iren <irasibil2@gmail.com>
2022-08-19 16:13:09 +0300

    preparation for authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sing-in/sign-in.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />} >
         <Route index element={<Home />} />
-        <Route index element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   ) 
