4bc1c6fb3fd13caa073b5992a8f451f236cac4fc
iren <irasibil2@gmail.com>
2022-08-22 10:36:19 +0300

    added toggle to cart component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,12 +6,14 @@ import CartDropdawn from "../../components/cart-dropdown/cart-dropdown.component
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 import { UserContext } from "../../context/user.context";
+import { CartContext } from "../../context/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import "./navigation.styles.scss";
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
     
     return (
         <Fragment>
@@ -33,7 +35,7 @@ const Navigation = () => {
                     )}
                     <CartIcon />
                 </div>
-                <CartDropdawn />
+                {isCartOpen && <CartDropdawn />}
             </div>
             <Outlet />
         </Fragment>
