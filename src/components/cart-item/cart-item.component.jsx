e0b4f9adffcb889ad64c2abec8a0c2fb4a78a747
iren <irasibil2@gmail.com>
2022-08-22 12:04:56 +0300

    added styles and created item counter

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,12 +1,15 @@
 import './cart-item.styles.scss';
 
 const CartItem = ({ cartItem }) => {
-    const { name, quantity } = cartItem;
+    const { name, imageUrl, price, quantity } = cartItem;
 
     return (
-        <div>
-            <h2>{name}</h2>
-            <span>{quantity}</span>
+        <div className='cart-item-container'>
+            <img src={imageUrl} alt={`${name}`} />
+            <div className="item-details">
+                <div className="name">{name}</div>
+                <span className='price'>{quantity} x ${price}</span>
+            </div>
         </div>
     )
 }
