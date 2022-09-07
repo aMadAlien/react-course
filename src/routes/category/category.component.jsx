a08bea978d6ebf06a9f50a1b94e63bd4afc231c2
iren <irasibil2@gmail.com>
2022-09-07 20:12:29 +0300

    improved categories-component with selector and dispatch

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,13 +1,14 @@
 import './category.styles.scss';
 import { useParams } from "react-router-dom";
-import { Fragment, useContext, useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { useSelector } from 'react-redux';
 
-import { CategoriesContext } from "../../context/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
+import { selectCategoriesMap } from '../../store/categories/category.selector';
 
 const Category = () => {
     const {category} = useParams();
-    const { categoriesMap } = useContext(CategoriesContext);
+    const categoriesMap = useSelector(selectCategoriesMap);
     const [products, setProducts] = useState(categoriesMap[category]); 
 
     useEffect(() => {
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,10 +2,28 @@ import './shop.styles.scss';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
+import { getCategoriesAndDocs } from '../../utils/firebase/firebase.utils';
+import { setCategoriesMap } from '../../store/categories/category.action';
 
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from "react";
+import { useDispatch } from 'react-redux';
 
 const Shop = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        // create getCategoriesMap to use as async/await fucn
+        const getCategoriesMap = async () => {
+            // receive the docs
+            const categoryMap = await getCategoriesAndDocs();
+            console.log(categoryMap);
+            // change state using received docs
+            dispatch(setCategoriesMap(categoryMap));
+        }
+
+        getCategoriesMap();
+    }, [])
 
     return (
         <Routes>
