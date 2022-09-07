f45f4b490b81301582909e52b5776da5220f3308
iren <irasibil2@gmail.com>
2022-09-07 19:00:59 +0300

    work with useDispatch()
    changed user.context to user reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+
 import { Routes, Route } from "react-router-dom";
+
+import { createUserDocFromAuth, onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 import Home from "./routes/home/home.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
+import { setCurrentUser } from "./store/user/user.action";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  // switching to the auth page check if user signed in or out to use correct settings
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) =>{
+      if (user) {
+        createUserDocFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />} >
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 
 import App from './App';
-import { UserProvider } from './context/user.context';
 import { CategoriesProvider } from './context/categories.context';
 import { CartProvider } from './context/cart.context';
 import { store } from './store/store';
@@ -16,13 +15,11 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <UserProvider>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
-        </UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
