75df57581adeca97252a862d7bdf5f0a2647e7f0
iren <irasibil2@gmail.com>
2022-09-08 11:04:18 +0300

    work with reselect

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,18 @@
-export const selectCategoriesMap = (state) => {
-    return state.categories.categories.reduce((acc, category) => {
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => 
+    categories.reduce((acc, category) => {
         const { title, items } = category.data();
         acc[title.toLowerCase()] = items;
         return acc;
-    }, {});
-}
+    }, {})
+);
