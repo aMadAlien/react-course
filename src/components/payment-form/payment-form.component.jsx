bb8fa6a6191c97bf6ca48c2c1faa5363855c3fd0
iren <irasibil2@gmail.com>
2022-09-21 16:01:27 +0300

    finished stripe payment

---
+++ src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,95 @@
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+
+import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component";
+
+import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
+
+const PaymentForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const paymentHandler = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+        return;
+    }
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+        method: "post",
+        headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ amount: 100 }),
+    }).then((res) => {
+        return res.json();
+    });
+
+    const clientSecret = response.paymentIntent.client_secret;
+
+    const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: elements.getElement(CardElement),
+            billing_details: {
+                name: "Yihua Zhang",
+            },
+        },
+    });
+
+
+    if (paymentResult.error) {
+        alert(paymentResult.error.message);
+    } else {
+        if (paymentResult.paymentIntent.status === "succeeded") {
+            alert("Payment Successful!");
+        }
+    }
+    // const stripe = useStripe();
+    // const elements = useElements();
+
+    // const paymentHandler = async (e) => {
+    //     e.preventDefault();
+    //     if (!stripe || !elements) {
+    //         return;
+    //     }
+
+    //     const response = await fetch('/.netlify/functions/create-payment-intent', {
+    //         method: 'post',
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify({ amount: 10000 }),
+    //     }).then(res => res.json());
+
+    //     const clientSecret = response.paymentIntent.client_secret;
+    //     console.log(clientSecret);
+
+    //     const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+    //         payment_method: {
+    //             card: elements.getElement(CardElement),
+    //             billing_details: {
+    //                 name: 'Iren'
+    //             },
+    //         },
+    //     });
+
+    //     if (paymentResult.error) {
+    //         alert(paymentResult.error.message);
+    //     } else {
+    //         if (paymentResult.paymentIntent.status === 'succeeded') {
+    //             alert('payment successful');
+    //         }
+    //     }
+    };
+
+    return (
+        <PaymentFormContainer>
+            <FormContainer onSubmit={paymentHandler}>
+                <h2>Credit Card Payment: </h2>
+                {/* display inputs for card details */}
+                <CardElement />
+                <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
+            </FormContainer>
+        </PaymentFormContainer>
+    );
+};
+
+export default PaymentForm;
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-import PaymentForm from '../../payment-form/payment-form.component';
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 import {
   CheckoutContainer,
